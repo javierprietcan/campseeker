cb8fc0fa7909c3d455ff8eaf22f66d8769e947a6
unknown <10.javierprieto.1a@gmail.com>
2021-12-16 22:39:13 +0100

    kasa

--- seeds/index.js
+++ seeds/index.js
@@ -7,7 +7,7 @@ if (process.env.NODE_ENV !== 'production') {
     require('dotenv').config();
 }
 
-const dbUrl = process.env.DB_URL || 'mongodb://localhost:27017/yelp-camp';
+const dbUrl = 'mongodb://localhost:27017/yelp-camp';
 
 
 mongoose.connect(dbUrl, {
