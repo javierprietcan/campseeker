22e8cc4853bff218b6465a4987a1538e4acb9bc0
unknown <10.javierprieto.1a@gmail.com>
2021-12-17 13:50:15 +0100

    reverse changes

--- seeds/index.js
+++ seeds/index.js
@@ -8,7 +8,7 @@ if (process.env.NODE_ENV !== 'production') {
 }
 
 // const dbUrl = process.env.DB_URL || 'mongodb://localhost:27017/yelp-camp';
-const dbUrl = 'mongodb://localhost:27017/yelp-camp';
+const dbUrl = process.env.DB_URL;
 
 
 mongoose.connect(dbUrl, {
